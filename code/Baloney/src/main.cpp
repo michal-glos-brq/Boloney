2f47a7902a61d9198fd4704b1d65333a0d94d867
R4sp1 <57303554+R4sp1@users.noreply.github.com>
2024-02-26 16:45:51 +0100

    Add MPU6050 library and implement reading and printing of raw data, lets see if it works...

--- code/Baloney/src/main.cpp
+++ code/Baloney/src/main.cpp
@@ -1,47 +1,17 @@
 #include <Arduino.h>
-#include <Wire.h>
-// inicializace proměnné pro určení adresy senzoru
-// 0x68 nebo 0x69, dle připojení AD0
-const int MPU_addr=0x68;
-// inicializace proměnných, do kterých se uloží data
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+#include "MPU6050.h"
 
-void setup()
-{
-  // komunikace přes I2C sběrnici
-  Wire.begin();
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x6B);
-  Wire.write(0);
-  Wire.endTransmission(true);
-  // komunikace přes sériovou linku rychlostí 115200 baud
-  Serial.begin(115200);
+MPU6050 mpu;
+
+void setup() {
+    mpu.begin();
 }
 
-void loop()
-{
-  // zapnutí přenosu
-  Wire.beginTransmission(MPU_addr);
-  // zápis do registru ACCEL_XOUT_H
-  Wire.write(0x3B);
-  Wire.endTransmission(false);
-  // vyzvednutí dat z 14 registrů
-  Wire.requestFrom(MPU_addr,14,true);
-  AcX=Wire.read()<<8|Wire.read();    
-  AcY=Wire.read()<<8|Wire.read();
-  AcZ=Wire.read()<<8|Wire.read();
-  Tmp=Wire.read()<<8|Wire.read();
-  GyX=Wire.read()<<8|Wire.read();
-  GyY=Wire.read()<<8|Wire.read();
-  GyZ=Wire.read()<<8|Wire.read();
-  // výpis surových dat z proměnných na sériovou linku
-  Serial.print("AcX = "); Serial.print(AcX);
-  Serial.print(" | AcY = "); Serial.print(AcY);
-  Serial.print(" | AcZ = "); Serial.print(AcZ);
-  // přepočtení teploty dle datasheetu
-  Serial.print(" | Temp = "); Serial.print(Tmp/340.00+36.53);
-  Serial.print(" | GyX = "); Serial.print(GyX);
-  Serial.print(" | GyY = "); Serial.print(GyY);
-  Serial.print(" | GyZ = "); Serial.println(GyZ);
-  delay(500);
+void loop() {
+    mpu.read();
+    mpu.printData();
+
+    MPU6050::struct_raw_MPU data = mpu.getRawData();
+    // Now you can access the raw data like this:
+    // data.AcX, data.AcY, data.AcZ, etc.
 }
